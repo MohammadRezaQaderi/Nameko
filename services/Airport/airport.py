fe637d32bb2f8594f6d763d9833b0ecd02c4117d
mohammadrezaqaderi <mrq112775@outlook.com>
2024-06-01 12:22:49 +0330

    edit: the airport service

--- services/Airport/airport.py
+++ services/Airport/airport.py
@@ -4,16 +4,19 @@ from nameko_redis import Redis
 
 
 class AirportsService:
-    name = "airports_service"
+    name = "trips_service"
     redis = Redis('development')
 
     @rpc
-    def get(self, airport_id):
-        airport = self.redis.get(airport_id)
-        return airport
+    def get(self, trip_id):
+        trip = self.redis.get(trip_id)
+        return trip
 
     @rpc
-    def create(self, airport):
-        airport_id = uuid.uuid4().hex
-        self.redis.set(airport_id, airport)
-        return airport_id
+    def create(self, airport_from_id, airport_to_id):
+        trip_id = uuid.uuid4().hex
+        self.redis.set(trip_id, {
+            "from": airport_from_id,
+            "to": airport_to_id
+        })
+        return trip_id
